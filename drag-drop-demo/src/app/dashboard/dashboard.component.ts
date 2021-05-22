3c8d6decb21fc5ad62863ce8e98fb50e02193b9d
lepotatochef <henry.ik@hotmail.com>
2021-05-22 08:58:51 -0400

    Fixing draging issue

--- drag-drop-demo/src/app/dashboard/dashboard.component.ts
+++ drag-drop-demo/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {CdkDragDrop, moveItemInArray,CdkDragStart} from '@angular/cdk/drag-drop';
+import {CdkDragDrop, moveItemInArray, CdkDragStart, CdkDropList, CdkDropListGroup} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-dashboard',
@@ -23,42 +23,49 @@ export class DashboardComponent implements OnInit {
   appCards = [
     {
       id: 1,
+      order:0,
       name: "amazon",
       appIconPath: '../../assets/icons/amazon.svg',
       appLinkUrl: 'www.amazon.com'
     },
     {
       id: 2,
+      order:1,
       name: "netflix",
       appIconPath: '../../assets/icons/netflix.png',
       appLinkUrl: 'www.netflix.com'
     },
     {
       id: 3,
+      order:2,
       name: "dropbox",
       appIconPath: '../../assets/icons/dropbox.png',
       appLinkUrl: 'www.dropbox.com'
     },
     {
       id: 4,
+      order:3,
       name: "google",
       appIconPath: '../../assets/icons/google.png',
       appLinkUrl: 'www.netflix.com'
     },
     {
       id: 5,
+      order:4,
       name: "microsoft",
       appIconPath: '../../assets/icons/microsoft.png',
       appLinkUrl: 'www.microsoft.com'
     },
     {
       id: 6,
+      order:5,
       name: "apple",
       appIconPath: '../../assets/icons/apple.svg',
       appLinkUrl: 'www.apple.com'
     },
     {
       id: 7,
+      order:6,
       name: "github",
       appIconPath: '../../assets/icons/github.svg',
       appLinkUrl: 'www.github.com'
@@ -68,8 +75,13 @@ export class DashboardComponent implements OnInit {
 
   public dragging: boolean = false;
 
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.appCards, event.previousIndex, event.currentIndex);
+  drop(event: CdkDragDrop<number>): void {
+    moveItemInArray(
+      this.appCards,
+      event.previousContainer.data,
+      event.container.data
+    );
+    console.log(this.appCards)
   }
 
   public handleDragStart(event: CdkDragStart): void {
